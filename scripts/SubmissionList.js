beb5602d04c1df5df04c75e2d54c47afbce6dc0c
Misty Blue <hellomistyblue@gmail.com>
2025-08-16 21:15:13 -0500

    finished assignment

--- scripts/SubmissionList.js
+++ scripts/SubmissionList.js
@@ -1,12 +1,27 @@
-const SubmissionList = async () => {
-    const response = await fetch("http://localhost:8088/submissions")
+export const SubmissionList = async () => {
+    const response = await fetch("http://localhost:8088/submissions?_expand=socioLocation")
     const submissions = await response.json()
 
     let html = `
-        <div id="survey-submission-list">
+        <div id="survey-submissions-list">
             <h2>Survey Submissions</h2>
             `
-           
+    // Generate HTML for each submission using .map()
+    const submissionsHTML = submissions.map(
+        (submission) => {
+            return `
+                <section class="survey-submission-container">
+                    <h2>Submission ${submission.id}</h2>
+                    <p>Owns Blue Jeans: ${submission.ownsBlueJeans}</p>
+                    <p>Location: ${submission.socioLocation.label}</p>
+                </section>
+            `
+        }
+    )
+
+    // Join the array of strings into a single string and add to our HTML
+    html += submissionsHTML.join("")
+
         html += `
             </div>
         `
--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,32 @@
 import { JeanChoices } from "./JeanChoices.js"
 import { LocationChoices } from "./LocationChoices.js"
 import { SubmissionButton } from "./SubmissionButton.js"
+import { SubmissionList } from "./SubmissionList.js"
 
 //get a reference to the container element in our HTML
 const container = document.querySelector("#container")
 
-
 //define a render() function that: Calls our JeanChoices() function to get the HTML & Updates the container's innerHTML with the generated HTML
-
 
 const render = async () => {
     const jeansHTML = JeanChoices()
     const locationsHTML = await LocationChoices()
     const buttonHTML = SubmissionButton()
-    
+    const submissionsHTML = await SubmissionList()
+
     container.innerHTML = `
         ${jeansHTML}
         ${locationsHTML}
         ${buttonHTML}
+        ${submissionsHTML}
     `
 }
 
+// Add an event listener for our custom event
+document.addEventListener("newSubmissionCreated", render)
+
 //we call render() to display our component when the page loads
-
 render()
-
 
 // Function to convert transient state to permanent state
 export const saveSurveySubmission = async () => {
